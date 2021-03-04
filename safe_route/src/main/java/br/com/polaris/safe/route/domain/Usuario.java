b088f862b70b368c214db6b81ef1f420bc82af3d
Larissa Custodio De Sousa <lcsousa3@stefanini.com>
2021-03-03 22:26:04 -0300

    Criação da classe ContatoConfianca e ContatoConfiancaController com seus respectivos endpoints

--- safe_route/src/main/java/br/com/polaris/safe/route/domain/Usuario.java
+++ safe_route/src/main/java/br/com/polaris/safe/route/domain/Usuario.java
@@ -1,15 +1,21 @@
 package br.com.polaris.safe.route.domain;
 
-public class Usuario {
+public abstract class Usuario {
+    private int id;
     private String nome;
     private String email;
     private String senha;
 
-    public Usuario(String nome, String email, String senha) {
+    public Usuario(int id, String nome, String email, String senha) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
     }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getNome() {
         return nome;
